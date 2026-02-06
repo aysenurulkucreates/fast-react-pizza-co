3da8db00d519d37fab78f08706c1847496c9d97e
aysenurulkucreates <aysenurulku17@gmail.com>
2026-02-06 14:35:38 +0300

    Writing Data With React Router "Actions"

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { Form, redirect } from "react-router-dom";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
@@ -35,46 +35,60 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <Form method="POST">
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required />
-        </div>
+      <div>
+        <label>First Name</label>
+        <input type="text" name="customer" required />
+      </div>
 
+      <div>
+        <label>Phone number</label>
         <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required />
-          </div>
+          <input type="tel" name="phone" required />
         </div>
+      </div>
 
+      <div>
+        <label>Address</label>
         <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required />
-          </div>
+          <input type="text" name="address" required />
         </div>
+      </div>
 
-        <div>
-          <input
-            type="checkbox"
-            name="priority"
-            id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
-          />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
-        </div>
+      <div>
+        <input
+          type="checkbox"
+          name="priority"
+          id="priority"
+          // value={withPriority}
+          // onChange={(e) => setWithPriority(e.target.checked)}
+        />
+        <label htmlFor="priority">Want to yo give your order priority?</label>
+      </div>
 
-        <div>
-          <button>Order now</button>
-        </div>
-      </form>
-    </div>
+      <div>
+        <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+        <button>Order now</button>
+      </div>
+    </Form>
   );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
+
+  const newOrder = await createOrder(order);
+
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
